c2f9131ccce8e544d8e0bbc7228f2f57ca35c831
Ryan Yogan <ryanyogan@gmail.com>
2025-03-31 16:41:54 -0500

    reset the pagination on search

--- app/features/tickets/components/ticket-search-input.tsx
+++ app/features/tickets/components/ticket-search-input.tsx
@@ -1,14 +1,27 @@
-import { useQueryState } from "nuqs";
+import { useQueryState, useQueryStates } from "nuqs";
 import { SearchInput } from "~/components/search-input";
-import { searchParser } from "../search-params";
+import {
+  paginationOptions,
+  paginationParser,
+  searchParser,
+} from "../search-params";
 
 export function TicketSearchInput({ placeholder }: { placeholder: string }) {
   const [search, setSearch] = useQueryState("search", searchParser);
+  const [pagination, setPagination] = useQueryStates(
+    paginationParser,
+    paginationOptions
+  );
+
+  function handleSearch(value: string) {
+    setPagination({ ...pagination, page: 0 });
+    setSearch(value);
+  }
 
   return (
     <SearchInput
       value={search}
-      onChange={setSearch}
+      onChange={handleSearch}
       placeholder={placeholder}
     />
   );
