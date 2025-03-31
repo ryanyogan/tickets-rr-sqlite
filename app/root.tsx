b90f7134618ce5a4ce665e11340dfffd74cb7b46
Ryan Yogan <ryanyogan@gmail.com>
2025-03-31 17:43:30 -0500

    maybe this font

--- app/root.tsx
+++ app/root.tsx
@@ -20,19 +20,6 @@ import { getUserFromSession } from "./lib/session.server";
 import { parseTheme } from "./lib/theme.server";
 import { clearToasts, getToasts } from "./lib/toast.server";
 
-export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
-
 export async function loader({ request }: Route.LoaderArgs) {
   const user = await getUserFromSession(request);
   const theme = await parseTheme(request);
@@ -52,14 +39,14 @@ export function Layout({ children }: { children: React.ReactNode }) {
   const theme = useTheme() === "dark" ? "dark" : "";
 
   return (
-    <html lang="en" className={`${theme} antialiased`}>
+    <html lang="en" className={`${theme} antialiased font-geist`}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body className="antialiased">
+      <body>
         <NuqsAdapter>
           <Header />
           <div className="flex h-screen overflow-hidden border-collapse">
