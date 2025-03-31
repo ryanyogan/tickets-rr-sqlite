03a5df76b09581d293ab1422381767975e11781f
Ryan Yogan <ryanyogan@gmail.com>
2025-03-31 16:53:12 -0500

    added size change of pagination

--- app/components/pagination.tsx
+++ app/components/pagination.tsx
@@ -1,4 +1,11 @@
 import { Button } from "./ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
 
 type PageAndSize = {
   page: number;
@@ -33,11 +40,14 @@ export function Pagination({
     onPagination({ ...pagination, page: pagination.page + 1 });
   }
 
+  function handleChangeSize(size: string) {
+    onPagination({ ...pagination, size: parseInt(size) });
+  }
+
   const previousButton = (
     <Button
       className="cursor-pointer"
       variant="outline"
-      size="sm"
       disabled={pagination.page < 1}
       onClick={handlePreviousPage}
     >
@@ -49,7 +59,6 @@ export function Pagination({
     <Button
       className="cursor-pointer"
       variant="outline"
-      size="sm"
       disabled={!hasNextPage}
       onClick={handleNextPage}
     >
@@ -57,10 +66,29 @@ export function Pagination({
     </Button>
   );
 
+  const sizeButton = (
+    <Select
+      defaultValue={pagination.size.toString()}
+      onValueChange={handleChangeSize}
+    >
+      <SelectTrigger className="h-[36px]">
+        <SelectValue />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="5">5</SelectItem>
+        <SelectItem value="10">10</SelectItem>
+        <SelectItem value="25">25</SelectItem>
+        <SelectItem value="50">50</SelectItem>
+        <SelectItem value="100">100</SelectItem>
+      </SelectContent>
+    </Select>
+  );
+
   return (
     <div className="flex justify-between items-center">
       <p className="text-sm text-muted-foreground">{label}</p>
       <div className="flex gap-x-2">
+        {sizeButton}
         {previousButton}
         {nextButton}
       </div>
