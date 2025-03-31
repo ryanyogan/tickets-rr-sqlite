0085145bc81a8276306c473190e42a0eb5341e24
Ryan Yogan <ryanyogan@gmail.com>
2025-03-31 16:47:26 -0500

    fixed interplay issues, reactive pagination reset

--- app/features/tickets/components/ticket-pagination.tsx
+++ app/features/tickets/components/ticket-pagination.tsx
@@ -1,6 +1,11 @@
-import { useQueryStates } from "nuqs";
+import { useQueryState, useQueryStates } from "nuqs";
+import { useEffect, useRef } from "react";
 import { Pagination } from "~/components/pagination";
-import { paginationOptions, paginationParser } from "../search-params";
+import {
+  paginationOptions,
+  paginationParser,
+  searchParser,
+} from "../search-params";
 
 type TicketPaginationProps = {
   paginatedTicketMetadata: {
@@ -16,6 +21,18 @@ export function TicketPagination({
     paginationParser,
     paginationOptions
   );
+
+  const [search] = useQueryState("search", searchParser);
+  const previousSearch = useRef(search);
+
+  useEffect(() => {
+    if (search === previousSearch.current) {
+      return;
+    }
+    previousSearch.current = search;
+
+    setPagination({ ...pagination, page: 0 });
+  }, [search, pagination, setPagination]);
 
   return (
     <Pagination
--- app/features/tickets/components/ticket-search-input.tsx
+++ app/features/tickets/components/ticket-search-input.tsx
@@ -1,27 +1,14 @@
-import { useQueryState, useQueryStates } from "nuqs";
+import { useQueryState } from "nuqs";
 import { SearchInput } from "~/components/search-input";
-import {
-  paginationOptions,
-  paginationParser,
-  searchParser,
-} from "../search-params";
+import { searchParser } from "../search-params";
 
 export function TicketSearchInput({ placeholder }: { placeholder: string }) {
   const [search, setSearch] = useQueryState("search", searchParser);
-  const [pagination, setPagination] = useQueryStates(
-    paginationParser,
-    paginationOptions
-  );
-
-  function handleSearch(value: string) {
-    setPagination({ ...pagination, page: 0 });
-    setSearch(value);
-  }
 
   return (
     <SearchInput
       value={search}
-      onChange={handleSearch}
+      onChange={setSearch}
       placeholder={placeholder}
     />
   );
